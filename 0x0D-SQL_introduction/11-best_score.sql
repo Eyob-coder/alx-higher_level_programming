63400635f8386b57e8bb6ae718ea530c530b2c36
Eyob <oddjobera@gmail.com>
2022-09-07 00:03:54 -0700

    list second_table score >= 0, descending

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- Script that lists all records of the table second_table of the database hbtn_0c_0
-SELECT score, name FROM second_table ORDER BY score>=10;
+SELECT score, name FROM second_table WHERE SCORE>=10 ORDER BY SCORE DESC;
