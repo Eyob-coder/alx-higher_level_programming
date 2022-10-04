9d225c5288c2e7e0a2dfcd23e022a577e7a64c53
Eyob <Eyob-coder@github.com>
2022-10-04 11:38:28 +0000

    undefined replacement fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 // class Square that defines a square and inherits from Rectangle,use x if c undefined
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
+const SquareP = require('./5-square');
 
+class Square extends SquareP {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
-    for (let column = 0; column < this.height; column++) {
-      console.log(c.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
     }
   }
 }
